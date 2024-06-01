9aa27a87a2afde4bdd418a43ca67ec6266b2ae3d
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-06-01 14:00:45 -0400

    Jump functionality added. Removed vertical input
    -Removed the ability to move the character on the vertical(y) axis.
    -Added a check, if the player presses the W key, force is added to the Y axis * _jumpForce variable, which propels the character up using the rigidbody2D.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,27 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 4.0f;
+    [SerializeField]
+    private float _jumpForce = 400.0f;
+
+    private Rigidbody2D _rb2D;
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         float _horizontal = Input.GetAxis("Horizontal");
-        float _vertical = Input.GetAxis("Vertical");
 
-        Vector3 _movement = new Vector3(_horizontal, _vertical);
+        Vector3 _movement = new Vector3(_horizontal, 0);
 
         transform.position += _movement * Time.deltaTime * _moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _rb2D.AddForce(Vector2.up * _jumpForce);
+        }
     }
 }
